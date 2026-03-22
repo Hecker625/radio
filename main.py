4d644e3d9ce7ff4b1f78164442f8a0ed18ee7847
Hecker625 <kyler.shah@gmail.com>
2026-03-22 14:19:32 -0700

    Create base code
    I was lazy and decided to use playsound because I was running the code in an online IDE

---
+++ main.py
@@ -0,0 +1,34 @@
+from playsound import playsound
+
+print("Playing KNKX 88.5")
+playsound("https://knkx-live-a.edge.audiocdn.com/6284_256k")
+
+print("Playing MOViN 92.5")
+playsound("https://playerservices.streamtheworld.com/api/livestream-redirect/KQMVFM.mp3")
+
+print("Playing EMMA 94.1")
+playsound("https://live.amperwave.net/direct/audacy-kswdfmaac-imc")
+
+print("Playing KUOW 94.9")
+playsound("https://playerservices.streamtheworld.com/api/livestream-redirect/KUOWFM_HIGH_MP3.mp3")
+
+print("Playing 95.7 The Jet")
+playsound("https://stream.revma.ihrhls.com/zc2569")
+
+print("Playing 96.5 Jack FM")
+playsound("https://stream.revma.ihrhls.com/zc7788")
+
+print("Playing KING 98.1")
+playsound("http://classicalking.streamguys1.com/king-fm-mp3")
+
+print("Playing KZOK 102.5")
+playsound("https://stream.revma.ihrhls.com/zc7787")
+
+print("Playing HITS 106.1")
+playsound("https://stream.revma.ihrhls.com/zc4257")
+
+print("Playing WARM 106.9")
+playsound("https://playerservices.streamtheworld.com/api/livestream-redirect/KRWMFM.mp3")
+
+print("Playing 107.7 THE END")
+playsound("https://live.amperwave.net/direct/audacy-knddfmaac-imc")
